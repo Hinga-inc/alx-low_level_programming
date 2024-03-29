ed4aa9c01809d8f2bd8fcbfa78d733c27492907a
HINGA PETER <141874604+Hinga-inc@users.noreply.github.com>
2024-03-29 21:40:27 +0300

    Add files via upload

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,16 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
- */
+ * _isupper - checks for uppercase letter
+ * @c : int
+ *
+ * _isupper: return 1 if uppercase
+ *
+ * Return: int
+ **/
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
