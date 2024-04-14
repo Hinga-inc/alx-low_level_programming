da4ab2c99597ee0a968c29414a1d779fde28f8f5
Hinga-inc <hingapeter18@gmail.com>
2024-04-14 07:41:21 -0700

    Write a hash function implementing the djb2 algorithm.
    Prototype: unsigned long int hash_djb2(const unsigned char *str);
    You are allowed to copy and paste the function from this page

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,7 @@
-unsigned long int hash_djb2(const unsigned char *str)
+#include "hash_tables.h"
+
+
+unsigned long int hash_djb2(const unsigned char * str)
 {
 	unsigned long int hash;
 	int c;
@@ -11,3 +14,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 	return (hash);
 }
 
+/**
+ * hash_djb2 - hash function djb2 style
+ * @str: key value
+ *
+ * Description: compute index with djb2 algorithm
+ * Return: hash code
+ */
